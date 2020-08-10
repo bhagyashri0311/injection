d3e5c39e8bd3e7a75bec750f7d4f7bf560f2a151
tukaram57 <bhosaletuks@ymail.com>
2020-08-10 17:25:54 +0100

    Injection

--- src/main/java/Test/ClientApplication.java
+++ src/main/java/Test/ClientApplication.java
@@ -7,7 +7,7 @@ public class ClientApplication {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DIConfiguration.class);
         MyApplication app = context.getBean(MyApplication.class);
-
+        System.out.println("hello world");
         app.processMessage("Hi Pankaj", "pankaj@abc.com");
 
         //close the context
